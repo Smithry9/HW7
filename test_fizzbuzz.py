652b851055c2298768b434d9411890bc40e91c53
Smithry9 <smithry9@oregonstate.edu>
2021-05-31 14:22:51 -0700

    Wrote first test, see if length of fizzbuzz output is 100

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,6 +3,9 @@ import fizzbuzz
 
 class testCaseAdd(unittest.TestCase):
     #test cases
+    def test_fizzbuzz1(self):
+        #makes sure fizzbuzz creates an output of length 100
+        self.assertEqual(len(fizzbuzz.fizzbuzz()),100)
     
 
 
