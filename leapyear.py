32a9b5440944ef6320479924d706a12e46fa940e
Smithry9 <smithry9@oregonstate.edu>
2021-05-31 15:00:39 -0700

    Implemented year divisible by 400 method

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,10 @@
 def leapyear(inputYear):
     if(inputYear % 4 == 0):
         if(inputYear % 100 == 0):
-            return False
+            if(inputYear % 400 == 0):
+                return True
+            else:
+                return False
         else:
             return True
     return False
