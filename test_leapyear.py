7b5bacad90c4793daeaac818e8e71b8c91f30791
Smithry9 <smithry9@oregonstate.edu>
2021-05-31 14:47:09 -0700

    Created skeleton file for test_leapyear and empty leapyear function

---
+++ test_leapyear.py
@@ -0,0 +1,15 @@
+import unittest
+import leapyear
+
+class testCaseAdd(unittest.TestCase):
+    #test cases
+    def test_fizzbuzz1(self):
+        #
+        self.assertEqual()
+
+    
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
