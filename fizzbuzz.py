bc2fda740bce6f7b6fd8e952298aca141176f3c3
Smithry9 <smithry9@oregonstate.edu>
2021-05-31 14:44:32 -0700

    Implemented write FizzBuzz instead of number method

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,14 @@
 def fizzbuzz():
     output = []
     for i in range(1,101):
-        if(i%3 == 0):
+        if(i%3 == 0 and i%5 == 0):
+            output.append("FizzBuzz")
+        elif(i%3 == 0):
             output.append("Fizz")
         elif(i%5 == 0):
             output.append("Buzz")
         else:
             output.append(i)
     return output
+
 
