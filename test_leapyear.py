2a46f4db3f8942876b114901a7b289b036ffac2e
Smithry9 <smithry9@oregonstate.edu>
2021-05-31 14:55:05 -0700

    Revert "Wrote first test, year divisible by 4 = True (testing with 24)"
    This reverts commit d3e45cade61a2c5e81cf5256adaa38d0c239f8f3.

--- test_leapyear.py
+++ test_leapyear.py
@@ -7,10 +7,6 @@ class testCaseAdd(unittest.TestCase):
         #year divisible by 4
         self.assertEqual(leapyear.leapyear(24), True)
 
-    def test_leapyear2(self):
-        #year divisible by 100
-        self.assertEqual(leapyear.leapyear(100), False)
-
     
     
 
